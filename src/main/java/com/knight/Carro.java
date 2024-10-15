f9c278701fe286dac6e01b0b17046e9534f71efc
Yago de Lima França <yagodlfa@gmail.com>
2024-10-15 09:18:08 -0300

    primeiro commit

---
+++ src/main/java/com/knight/Carro.java
@@ -0,0 +1,5 @@
+package com.knight;
+
+public class Carro {
+
+}
---
+++ src/main/java/com/knight/Main.java
@@ -0,0 +1,9 @@
+package com.knight;
+
+public class Main {
+
+    String nome = "Bruce";
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
