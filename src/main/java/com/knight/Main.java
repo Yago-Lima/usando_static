addbe3afa406cc560aec4a088c2e0505bbb66460
Yago de Lima França <yagodlfa@gmail.com>
2024-10-15 09:25:51 -0300

    exemplo de referência non-static

--- src/main/java/com/knight/Main.java
+++ src/main/java/com/knight/Main.java
@@ -2,8 +2,19 @@ package com.knight;
 
 public class Main {
 
+    /**
+     * Este atributo pertence a qualquer objeto que seja criado a partir dessa classe
+     */
     String nome = "Bruce";
     public static void main(String[] args) {
-        System.out.println();
+        Main main = new Main(); // objeto criado a partir da classe teste
+        main.ola();
+    }
+
+    /**
+     * Este metodo pertence a qualquer objeto que seja criado a partir dessa classe
+     */
+    void ola(){
+        System.out.println("Olá "+nome);
     }
 }
