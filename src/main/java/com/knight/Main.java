743e90f0990694ed838f3c712426c267935c7c46
Yago de Lima França <yagodlfa@gmail.com>
2024-10-15 09:44:15 -0300

    exemplo de efeitos colaterais

--- src/main/java/com/knight/Main.java
+++ src/main/java/com/knight/Main.java
@@ -4,8 +4,16 @@ public class Main {
 
 
     public static void main(String[] args) {
+
+        /*
+        * Como efeito colateral de usar static em um atributo,
+        * sempre que eu altera-lo todos os objetos da classe
+        * que o contém serão alterados ex:
+        * */
         Carro carro1 = new Carro();
         Carro carro2 = new Carro();
+
+        Carro.definicao = "Veiculo sobre rodas que voa";
         carro2.cor = "amarelo";
 
         /*
